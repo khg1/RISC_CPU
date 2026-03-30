07955856a6293fa8ceee912cf5e961b6a3c5b487
khg9056 <khg9056@ecs02.poly.edu>
2026-03-29 23:54:06 -0400

    compatible to multiple tests

--- simulation/run.f
+++ simulation/run.f
@@ -1,18 +1,6 @@
-
-     -64
-     -sv
-     -access +rwc
-     #-gui
-     -timescale 1ns/1ps
      +incdir+../rtl
      -y ../rtl
+     -y ../tb
      +libext+.sv
-     +PROG=../tb/program_fib.hex
-     +DATA=../tb/data_fib.hex
-     +GOLDREG=../tb/gold_rf_fib.hex
-     +GOLDMEM=../tb/gold_data_fib.hex
-     ../tb/tb_top.sv
-     ../sva/axi_read_assertions.sv
-     -assert
 
 
